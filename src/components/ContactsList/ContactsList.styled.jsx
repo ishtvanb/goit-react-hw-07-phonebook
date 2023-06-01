7456de11ad60bc4edbb3c0ac21d6a1f0c70715e9
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-01 19:35:04 +0300

    Updated Phonebook

---
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const ContactItem = styled.li`
+  display: flex;
+  align-items: center;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  border-radius: 10px;
+  padding: 10px;
+  background-color: lightblue;
+  
+  &:not(:last-of-type) {
+    margin-bottom: 10px;
+  }
+
+&:hover {
+  background-color: darkgrey;
+  }
+`;
+
