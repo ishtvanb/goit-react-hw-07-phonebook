86aae8f1db33a107f3adc1f4a64f5de4b6065e6e
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-01 19:21:06 +0300

    Updated Phonebook

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,9 @@
-import { Formik, Field } from 'formik';
+import { Formik, Field, getIn } from 'formik';
 import toast, { Toaster } from 'react-hot-toast';
 import * as Yup from 'yup';
-import 'yup-phone-lite'
+import 'yup-phone-lite';
 import { useDispatch, useSelector } from 'react-redux';
-import { addContact } from 'redux/contactsSlice';
+import { addContact } from 'redux/operations';
 import {
   Form,
   FormField,
@@ -12,33 +12,43 @@ import {
   FormLabel,
   ErrorMessage,
   FormButton,
+  BsFillPersonFill,
+  RiPhoneLine,
 } from './ContactForm.styled';
-import { BsFillPersonFill } from 'react-icons/bs';
-import { RiPhoneLine } from 'react-icons/ri';
-import { getContacts } from 'redux/selectors';
+import { selectContacts } from 'redux/selectors';
 
 const ContactSchema = Yup.object({
   name: Yup.string()
     .min(2, 'Too Short!')
     .max(50, 'Too Long!')
     .required('Name is required!'),
-  number: Yup.string().phone('UA').required('Phone number is required!'),
+  phone: Yup.string().phone('UA').required('Phone number is required!'),
 });
 
 export const ContactForm = () => {
   const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
+  const contacts = useSelector(selectContacts);
+
+  function getStyles(errors, touched, fieldName) {
+    if (getIn(errors, fieldName) && getIn(touched, fieldName)) {
+      return {
+        borderColor: 'red',
+      };
+    }
+    return {};
+  }
+
   return (
     <>
       <Toaster />
       <Formik
         initialValues={{
           name: '',
-          number: '',
+          phone: '',
         }}
 
       validationSchema={ContactSchema}
-          onSubmit={(values, actions) => {
+        onSubmit={({ ...values }, actions) => {
           const existingContact = contacts.find(
             contact => contact.name.toLowerCase() === values.name.toLowerCase()
           );
@@ -48,53 +58,59 @@ export const ContactForm = () => {
             );
             actions.resetForm();
           } else {
-            dispatch(addContact(values.name, values.number));
+            dispatch(addContact({ ...values }));
             actions.resetForm();
           }
         }}
       >
-      <Form>
-        <FormField>
-          <FormLabel>Name</FormLabel>
-          <Cover>
-            <Field name="name">
-              {({ field }) => {
-                return <Input {...field} placeholder="your name" />;
-              }}
-            </Field>
-
-            <BsFillPersonFill
-              style={{
-                position: 'absolute',
-                top: '50%',
-                left: '10px',
-                transform: 'translateY(-50%)',
-              }}
-            />
-          </Cover>
+      {formikProps => (
+        <Form>
+          <FormField>
+            <FormLabel htmlFor="name">Name</FormLabel>
+              <Cover>
+                <Field name="name"
+                  render={({ field }) => (
+                    <Input 
+                      {...field}
+                      placeholder="your name"
+                      id="name"
+                      style={getStyles(
+                      formikProps.errors,
+                      formikProps.touched,
+                      'name'
+                      )}
+                     />
+                    )}
+                  />
+                <BsFillPersonFill />
+              </Cover>
           <ErrorMessage name="name" component="div" />
         </FormField>
         <FormField>
-          <FormLabel>Number</FormLabel>
-          <Cover>
-            <Field name="number">
-              {({ field }) => {
-                return <Input {...field} placeholder="+38-0xx-xxx-xx-xx" />;
-              }}
-            </Field>
-            <RiPhoneLine
-              style={{
-                position: 'absolute',
-                top: '50%',
-                left: '10px',
-                transform: 'translateY(-50%)',
-              }}
-            />
+          <FormLabel htmlFor="name">Number</FormLabel>
+           <Cover>
+                <Field
+                  name="phone"
+                  render {({ field }) => (
+                    <Input
+                      {...field} 
+                      placeholder="+38-0xx-xxx-xx-xx" 
+                      id="number"
+                        style={getStyles(
+                        formikProps.errors,
+                        formikProps.touched,
+                        'phone'
+                       )}
+                      />
+                    )}
+                  />
+            <RiPhoneLine />
           </Cover>
-          <ErrorMessage name="number" component="div" />
+          <ErrorMessage name="phone" component="div" />
         </FormField>
         <FormButton type="submit">Add contact</FormButton>
       </Form>
+      )}
       </Formik>
     </>
   );
---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,75 @@
+import styled from '@emotion/styled';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { RiPhoneLine } from 'react-icons/ri';
+import { Form as FormikForm, ErrorMessage as FormikErrorMessage } from 'formik';
+
+export const Form = styled(FormikForm)`
+  display: flex;
+  flex-direction: column;
+  background-color: lightblue;
+
+  margin-bottom: 20px;
+  padding: 20px;
+  box-shadow: rgba(70, 70, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+  border-radius: 7px;
+`;
+
+export const FormField = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+  width: 100%;
+`;
+
+export const FormLabel = styled.label`
+margin-bottom: 10px;
+`;
+
+export const Cover = styled.div`
+position: relative;
+&:focus-within {
+    svg {
+      transition: fill 250ms cubic-bezier(0.4, 0, 0.2, 1);
+      fill: yellowgreen;
+    }
+  }
+`;
+
+export const BsFillPersonFill = styled(BsFillPersonFill)`
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+`;
+
+export const RiPhoneLine = styled(RiPhoneLine)`
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+`;
+
+export const Input = styled.input`
+  height: 30px;
+  width: 88%;
+  padding-left: 30px;
+  border: 1px solid rgba(33, 33, 33, 0.2);
+  border-radius: 7px;
+`;
+
+export const ErrorMessage = styled(FormikErrorMessage)`
+  font-size: 14px;
+  color: red;
+`;
+
+export const FormButton = styled.button`
+  display: block;
+  margin: 0 auto;
+  padding: 10px;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  text-align: center;
+  background-color: limegreen;
+`;
