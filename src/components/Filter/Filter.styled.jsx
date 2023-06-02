c58aca0b840b13597672b9ff9b315c8c7fca2cb5
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-02 16:41:24 +0300

    Update Filter.styled.jsx

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -3,12 +3,14 @@ import styled from "@emotion/styled";
 export const FilterLabel = styled.label`
   display: block;
   margin-bottom: 10px;
+  margin-left: 70px;
 `;
 
 export const FilterInput = styled.input`
   display: block;
   height: 25px;
   margin-bottom: 20px;
+  margin-left: 60px;
   border: 3px solid deepskyblue;
   border-radius: 10px;
 `;
