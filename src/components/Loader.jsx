592eac06c79d29c4b0782987ef61ba6f75e5e42b
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-06-02 17:11:39 +0300

    Update Loader.jsx

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -7,7 +7,7 @@ const Loader = () => {
   height="80"
   width="80"
   ariaLabel="blocks-loading"
-  wrapperStyle={{}}
+      wrapperStyle={{}}
   wrapperClass="blocks-wrapper"
   colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
 />
